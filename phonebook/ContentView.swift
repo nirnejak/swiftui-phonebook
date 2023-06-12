6c0fb72aac2a6c1a70206c0b9840e640561fc9fc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-06-12 09:15:27 +0530

    Struct data used for contacts

--- phonebook/ContentView.swift
+++ phonebook/ContentView.swift
@@ -10,6 +10,59 @@ import SwiftUI
 struct ContentView: View {
     @State var appTitle = "Hello there!"
     @State var subtitle = "Building my first SwiftUI app"
+    
+    struct Contact {
+        var name: String
+        var role: String
+        var age: Int
+        
+        var twitter: String
+        var github: String
+        var dribbble: String
+    }
+    
+    @State var contacts = [
+        Contact(
+            name: "Walter White",
+            role: "Chemistry Teacher",
+            age: 51,
+            twitter: "https://twitter.com/walterwhite",
+            github: "https://github.com/walterwhite",
+            dribbble: "https://dribbble.com/walterwhite"
+        ),
+        Contact(
+            name: "Jessie Pinkman",
+            role: "Unemployed",
+            age: 25,
+            twitter: "https://twitter.com/jessie",
+            github: "https://github.com/jessie",
+            dribbble: "https://dribbble.com/jessie"
+        ),
+        Contact(
+            name: "Saul Goodman",
+            role: "Lawyer",
+            age: 38,
+            twitter: "https://twitter.com/saulgoodman",
+            github: "https://github.com/saulgoodman",
+            dribbble: "https://dribbble.com/saulgoodman"
+        ),
+        Contact(
+            name: "Hank Schrader",
+            role: "DEA Agent",
+            age: 48,
+            twitter: "https://twitter.com/jessie",
+            github: "https://github.com/jessie",
+            dribbble: "https://dribbble.com/jessie"
+        ),
+        Contact(
+            name: "Gustavo Fring",
+            role: "Businessman",
+            age: 57,
+            twitter: "https://twitter.com/gustavofring",
+            github: "https://github.com/gustavofring",
+            dribbble: "https://dribbble.com/gustavofring"
+        ),
+    ]
 
     var body: some View {
         ZStack {
@@ -33,15 +86,16 @@ struct ContentView: View {
                         .padding(.top)
                     
                     VStack(spacing: -50.0) {
-                        Card()
-                        Card()
-                        Card()
-                        Card()
-                        Card()
-                        Card()
-                        Card()
-                        Card()
-                        Card()
+                        ForEach(0 ..< contacts.count, id: \.self) { index in
+                            Card(
+                                name: contacts[index].name,
+                                role: contacts[index].role,
+                                age: contacts[index].age,
+                                twitter: contacts[index].twitter,
+                                dribbble: contacts[index].dribbble,
+                                github: contacts[index].github
+                            )
+                        }
                     }
                 }
             }
@@ -49,22 +103,30 @@ struct ContentView: View {
     }
     
     @ViewBuilder
-    func Card() -> some View {
+    func Card(
+        name: String,
+        role: String,
+        age: Int,
+        twitter: String,
+        dribbble: String,
+        github: String
+    ) -> some View {
         VStack {
             VStack(alignment: .leading) {
-                Text("John Doe")
+                Text(name)
                     .font(.title3).fontWeight(.semibold)
                     .padding(.top)
-                Text("Designer & Developer")
+                
+                Text("\(age), \(role)")
                     .font(.callout).fontWeight(.regular)
                     .padding(.top, 1.0)
                 
                 Divider().padding(.vertical, 5.0)
                 
                 HStack(spacing: 20.0) {
-                    Text("Twitter").font(.footnote)
-                    Text("Dribbble").font(.footnote)
-                    Text("Github").font(.footnote)
+                    Text("[Twitter](\(twitter))").font(.footnote)
+                    Text("[Dribbble](\(dribbble))").font(.footnote)
+                    Text("[Github](\(github))").font(.footnote)
                 }.padding(.vertical, 5.0)
                 
             }
@@ -91,6 +153,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
